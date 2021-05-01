0ec7ef97972eb1b15f1c513ef1f26b3663df96a2
phani.2205 <phani.2205@gmail.com>
2021-05-02 01:59:46 +0530

    changing nav up image based on the nav destination

--- app/src/main/java/com/app/newsfeed/MainActivity.kt
+++ app/src/main/java/com/app/newsfeed/MainActivity.kt
@@ -3,6 +3,7 @@ package com.app.newsfeed
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.app.newsfeed.databinding.ActivityMainBinding
 import com.app.newsfeed.utils.FortnightlyToolbar
 import kotlinx.android.synthetic.main.layout_toolbar.*
@@ -15,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         appBar = toolbar
+        val navController = Navigation.findNavController(this,R.id.newsNavHost)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.newsBulletinFragment){
+                menu.setImageResource(R.drawable.vd_menu)
+            }else{
+                menu.setImageResource(R.drawable.vd_back)
+            }
+        }
     }
 
 
